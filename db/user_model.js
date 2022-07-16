49371baf7975b13a2ad990b3161db6b7f015c629
coreybarker <cozybarker@gmail.com>
2022-07-16 16:54:32 +1000

    finished implementing user auth

--- db/user_model.js
+++ db/user_model.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("./connection");
+const UserModel = mongoose.model(
+  "User",
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-  },
-},
-{
-timestamps: true,
-}
-)
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: [true, "Please add a name"],
+      },
+      email: {
+        type: String,
+        required: [true, "Please add an email"],
+      },
+      password: {
+        type: String,
+        required: [true, "Please add a password"],
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = UserModel;
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const PostModel = require("../db/post_model");
-const UserModel = require("../db/user_model")
+const UserModel = require("../db/user_model");
 const { protect } = require("../middleware/authMiddleware");
+const asyncHandler = require("express-async-handler");
 
 // Posts-page get request.
 router.get("/", async (req, res) => {
@@ -28,7 +29,7 @@ router.get("/:id", async (req, res) => {
 router.post("/", protect, async (req, res) => {
   // creates new instance in memory and saves to database // .create returns a promise(could use '.then' but we're using async/await)
   PostModel.create(req.body, (err, doc) => {
-    user: req.user.id
+    user: req.user.id;
     if (err) {
       res.status(422).send({ error: err.message });
     } else {
@@ -38,46 +39,70 @@ router.post("/", protect, async (req, res) => {
 });
 
 // UPDATE REQUESTS //
-router.put("/:id", async (req, res) => {
-  const user = await UserModel.findById(req.user.id)
+router.put(
+  "/:id",
+  protect,
+  asyncHandler(async (req, res) => {
+    const post = await PostModel.findById(req.params.id);
+    const user = await UserModel.findById(req.user.id);
 
-  //Check for user
-  if (!user) {
-    res.status(401)
-    throw new Error('User not found')
-  }
+    //Check for user
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
 
-  // Make sure the logged in user matches the user who created the post
-  if (post.user.toString() !== user.id) {
-    res.status(401)
-    throw new Error('User not authorized')
-  }
+    // Make sure the logged in user matches the user who created the post through user id
+    if (post.user.toString() !== user.id) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
 
-  // findByIdAndUpdate 1st param is the one it finds, 2nd param is what it replaces it with. 3rd is options, 4th is (err, doc)
-  // returnDocument: 'after' the request otherwise it just returns the old value
-  PostModel.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { returnDocument: "after" },
-    (err, doc) => {
+    // findByIdAndUpdate 1st param is the one it finds, 2nd param is what it replaces it with. 3rd is options, 4th is (err, doc)
+    // returnDocument: 'after' the request otherwise it just returns the old value
+    PostModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { returnDocument: "after" },
+      (err, doc) => {
+        if (err) {
+          res.status(422).send({ error: err.message });
+        } else {
+          res.status(201).send(doc);
+        }
+      }
+    );
+  })
+);
+
+// DELETE REQUESTS //
+router.delete(
+  "/:id",
+  protect,
+  asyncHandler(async (req, res) => {
+    const post = await PostModel.findById(req.params.id);
+    const user = await UserModel.findById(req.user.id);
+
+    //Check for user
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    // Make sure the logged in user matches the user who created the post
+    if (post.user.toString() !== user.id) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+
+    PostModel.findByIdAndDelete(req.params.id, (err, doc) => {
       if (err) {
         res.status(422).send({ error: err.message });
       } else {
-        res.status(201).send(doc);
+        res.sendStatus(204);
       }
-    }
-  );
-});
-
-// DELETE REQUESTS //
-router.delete("/:id", async (req, res) => {
-  PostModel.findByIdAndDelete(req.params.id, (err, doc) => {
-    if (err) {
-      res.status(422).send({ error: err.message });
-    } else {
-      res.sendStatus(204);
-    }
-  });
-});
+    });
+  })
+);
 
 module.exports = router;
